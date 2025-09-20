d91412e9d72b03e080400e55fbe0c56bfdd7df39
andresmoraless <mor24120@byui.edu>
2025-09-19 19:50:37 -0600

    JS practice

---
+++ ponder/week1.js
@@ -0,0 +1,14 @@
+const PI = 3.14;
+let radius = 3;
+
+const one = 1;
+const two = '2';
+
+let course = "CSE131"; 
+if (true) {
+    let student = "John";
+    console.log(course);
+    console.log(student);
+}
+
+console.log(course);
