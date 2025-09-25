ae5dd19b0f89220b9b9e34b5f7dd515180f99f46
andresmoraless <mor24120@byui.edu>
2025-09-25 13:14:22 -0600

    change

--- ponder/dom-basics.js
+++ ponder/dom-basics.js
@@ -7,6 +7,10 @@ newImage.setAttribute("src", "https://picsum.photos/200");
 newImage.setAttribute("alt", "Random image from picsum.");
 document.body.appendChild(newImage);
 
+const newDiv = document.createElement("div");
+newDiv.innerHTML = "<ul><li>One</li><li>Two</li><li>Three</li></ul>";
+document.body.appendChild(newDiv);
+
 const newSection = document.createElement("section");
 const newH2 = document.createElement("h2");
 newH2.innerText = "DOM Basics";
