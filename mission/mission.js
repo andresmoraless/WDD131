da5ecef3d857b34331560f40e90415ebd67d773d
andresmoraless <mor24120@byui.edu>
2025-09-26 23:40:49 -0600

    theme

---
+++ mission/mission.js
@@ -0,0 +1,15 @@
+const themeSelector = document.querySelector("#theme");
+themeSelector.addEventListener("change", changeTheme);
+
+function changeTheme() {
+    const body = document.body;
+    const logo = document.getElementById("logo");
+
+if (themeSelector.value === "dark") {
+    body.classList.add("dark");
+    logo.src = "byui-logo_white.png";
+} else {
+    body.classList.remove("dark");
+    logo.src = "logo-blue.png";
+}
+}
