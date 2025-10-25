ae8f80ef2bdc208e162e4277cf4491fedc9cd767
andresmoraless <mor24120@byui.edu>
2025-10-25 17:56:30 -0600

    changes

--- prove/blog/blog.js
+++ prove/blog/blog.js
@@ -23,5 +23,45 @@ const articles = [
 		ages: '12-16',
 		genre: 'Fantasy',
 		stars: '⭐⭐⭐⭐'
+	},
+	{
+		id: 3,
+		title: "Belgariad Book One: Pawn of Prophecy",
+		date: "Feb 12, 2022",
+		description:
+		"A fierce dispute among the Gods and the theft of a powerful Orb leaves the World divided into five kingdoms. Young Garion, with his 'Aunt Pol' and an elderly man calling himself Wolf --a father and daughter granted near-immortality by one of the Gods -- set out on a complex mission.",
+		imgSrc:
+		"https://images-na.ssl-images-amazon.com/images/I/41ZxXA+nInL.jpg",
+		imgAlt: "Book cover for Pawn of Prophecy",
+		ages: "12-16",
+		genre: "Fantasy",
+		stars: "⭐⭐⭐⭐⭐"
 	}
-]
+];
+
+function articleTemplate(article, index) {
+	return `
+	  <section class="book-info${index + 1}">
+		<ul>
+		  <li id="date">${article.date}</li>
+		  <li>${article.ages}</li>
+		  <li>${article.genre}</li>
+		  <li>${article.stars}</li>
+		</ul>
+	  </section>
+  
+	  <section class="book" id="book${article.id}">
+		<h2>${article.title}</h2>
+		<img src="${article.imgSrc}" alt="${article.imgAlt}">
+		<p>${article.description}</p>
+	  </section>
+	`;
+  }
+  
+  const htmlStrings = articles.map(articleTemplate);
+  
+  document.querySelector('#maincontent').innerHTML =
+	htmlStrings.join('') +
+	`<section class="form"><p>Filters will go here</p></section>`;
+  
+  console.log('Rendered articles:', htmlStrings);
